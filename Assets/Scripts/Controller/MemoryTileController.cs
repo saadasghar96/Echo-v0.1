a075a49f60b3702aa63c2084dbb79039d2ecf42a
Saad Asghar <saadasghar.96@gmail.com>
2022-06-18 21:28:20 +0500

    Touch Controller v2
    Completed touch controller to detect touch on memory tile

--- Assets/Scripts/Controller/MemoryTileController.cs
+++ Assets/Scripts/Controller/MemoryTileController.cs
@@ -2,24 +2,58 @@
 
 public class MemoryTileController : MonoBehaviour
 {
-    RaycastHit2D hit;
-    Vector2[] touches = new Vector2[5];
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     // Update is called once per frame
     void Update()
     {
+        CheckForTouch();
+    }
+
+    // Check if a particular memory tile is touched
+    void CheckForTouch()
+    {
+#if !UNITY_EDITOR
         if (Input.touchCount > 0)
         {
-            foreach (Touch t in Input.touches)
-            {
-                touches[t.fingerId] = Camera.main.ScreenToWorldPoint(Input.GetTouch(t.fingerId).position);
-                if (Input.GetTouch(t.fingerId).phase == TouchPhase.Began)
-                    hit = Physics2D.Raycast(touches[t.fingerId], Vector2.zero);
-                if (hit.collider.name == "Tri(Clone)")
-                    hit.transform.position = touches[t.fingerId];
+            Vector3 screenPoint = Input.GetTouch(0).position;
 
-                print("Ouch");
+            // The z position is in world units from the camera
+            screenPoint.z = Vector3.Distance(transform.position, Camera.main.transform.position);
+
+            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(screenPoint);
+
+            bool overSprite = spriteRenderer.bounds.Contains(worldPoint);
+
+            if (overSprite)
+            {
+                this.gameObject.SetActive(false);
             }
         }
+#endif
+
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 screenPoint = Input.mousePosition;
+
+            // The z position is in world units from the camera
+            screenPoint.z = Vector3.Distance(transform.position, Camera.main.transform.position);
+
+            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(screenPoint);
+
+            bool overSprite = spriteRenderer.bounds.Contains(worldPoint);
+
+            if (overSprite)
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+#endif
     }
 }
